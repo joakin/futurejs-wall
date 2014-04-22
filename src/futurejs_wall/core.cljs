addcdc2ef5f3e25f0878992644edff0a8079fbb5
Joakin <joaquin@chimeces.com>
2014-04-22 18:39:26 +0200

    Initial commit. Moved output to web/

---
+++ src/futurejs_wall/core.cljs
@@ -0,0 +1,5 @@
+(ns futurejs-wall.core)
+
+(enable-console-print!)
+
+(println "Hello world!")
---
+++ web/out/futurejs_wall/core.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 0.0-2173
+goog.provide('futurejs_wall.core');
+goog.require('cljs.core');
+cljs.core.enable_console_print_BANG_.call(null);
+cljs.core.println.call(null,"Hello world!");
+
+//# sourceMappingURL=core.js.map
