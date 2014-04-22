6691e9285357968d27b3745658d39d97b25c683f
Joakin <joaquin@chimeces.com>
2014-04-22 19:05:39 +0200

    Resize canvas to full size and background

--- src/futurejs_wall/core.cljs
+++ src/futurejs_wall/core.cljs
@@ -5,12 +5,18 @@
 
 (println "Hello world!")
 
+(def window-width (.-innerWidth js/window))
+(def window-height (.-innerHeight js/window))
+
 (def canvas-dom (.getElementById js/document "canvas"))
+
+(set! (.-width canvas-dom) window-width)
+(set! (.-height canvas-dom) window-height)
 
 (def monet-canvas (canvas/init canvas-dom "2d"))
 
 (canvas/add-entity monet-canvas :background
-                   (canvas/entity {:x 0 :y 0 :w 600 :h 600} ; val
+                   (canvas/entity {:x 0 :y 0 :w window-width :h window-height} ; val
                                   nil                       ; update function
                                   (fn [ctx val]             ; draw function
                                     (-> ctx
